5c4a33232f8b03ec0a4fc0440b3ce0cb428121f8
wahab8 <wahab@unblur.co>
2023-03-21 23:08:45 +0100

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment.development';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'ecothreads-8fe49',
+    appId: '1:1083821577382:web:9b392307c7ad37181e1067',
+    storageBucket: 'ecothreads-8fe49.appspot.com',
+    apiKey: 'AIzaSyDvA-ONGqUms46vVtqC9sNfwktzhCNMTtY',
+    authDomain: 'ecothreads-8fe49.firebaseapp.com',
+    messagingSenderId: '1083821577382',
+    measurementId: 'G-ENDQY497NC',
+  },};
